dee9a6628ad059b96d4a499b4971ac5a34ec5b93
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-28 15:51:38 -0300

    feat: createUserRequest

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
+use App\Http\Requests\createUserRequest;
 
 class UserController extends Controller
 {
@@ -22,13 +23,9 @@ class UserController extends Controller
         return new UserResource($user);
     }
 
-    public function store(Request $request)
+    public function store(createUserRequest $request)
     {
-        $data = $request->validate([
-            'name' => 'required',
-            'email' => 'email|required',
-            'password' => 'required'
-        ]);
+        $data = $request->validated();
 
         if (User::where('email', $data['email'])->exists()) {
             return response()->json(['error' => 'Email already exists'], 400);
@@ -37,5 +34,25 @@ class UserController extends Controller
         $user = User::create($data);
 
         return response()->json($user, 201);
+    }
+
+    public function update(Request $request)
+    {
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+
+        $user->update($request->all());
+
+        return response()->json($user, 200);
+    }
+
+    public function destroy()
+    {
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+
+        $user->delete();
+
+        return response()->json(null, 204);
     }
 }
---
+++ server/app/Http/Requests/createUserRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class createUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if(auth()->user()){
+            return true;
+        } else {
+            return false;
+       }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:191',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório',
+            'name.string' => 'O campo nome deve ser uma string',
+            'name.max' => 'O campo nome deve ter no máximo 191 caracteres',
+            'email.required' => 'O campo email é obrigatório',
+            'email.email' => 'O campo email deve ser um email válido',
+            'email.unique' => 'O campo email já está em uso',
+            'password.required' => 'O campo senha é obrigatório',
+            'password.string' => 'O campo senha deve ser uma string',
+            'password.min' => 'O campo senha deve ter no mínimo 6 caracteres'
+        ];
+    }
+}
