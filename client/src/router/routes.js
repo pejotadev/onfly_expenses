2b1fac1262529b4cce955142e2247bc12cab63f2
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-29 13:32:02 -0300

    feat: route to expenses

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,6 +5,7 @@ import ExpensesManagerPage from '../pages/ExpensesManagerPage.vue';
 const routes = [
   { path: '/login', component: LoginPage },
   { path: '/', component: HomePage, meta: { requiresAuth: true } },
+  { path: '/expenses', component: ExpensesManagerPage, meta: { requiresAuth: true } },
   {
     path: '/:catchAll(.*)*',
     component: () => import('../pages/ErrorNotFound.vue')
